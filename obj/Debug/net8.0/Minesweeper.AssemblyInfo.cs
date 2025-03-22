a5ba257eb2af5d9f50482487891d16ffb9664cc5
SatoriSh <satorizoom51@gmail.com>
2025-03-22 14:14:42 +0100

    main assemply info

--- obj/Debug/net8.0/Minesweeper.AssemblyInfo.cs
+++ obj/Debug/net8.0/Minesweeper.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Minesweeper")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+af4940d1b276cdf74a67c08a3c2564df667c5c99")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d6277f6d672c6256fa6f0e7103f3b37d74fbf36b")]
 [assembly: System.Reflection.AssemblyProductAttribute("Minesweeper")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Minesweeper")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
